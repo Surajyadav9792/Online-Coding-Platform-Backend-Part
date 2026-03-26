be5326cb96de07f17bfa116360755e3a073c9031
Suraj Yadav <surajyadav2823510@gmail.com>
2026-03-26 19:09:20 +0530

    AI chat code updated

--- controllers/Gemini.js
+++ controllers/Gemini.js
@@ -2,13 +2,14 @@
 const { GoogleGenAI }= require("@google/genai");
 const solveDout=async(req,res)=>{
     try{
+      const { messages ,title,description,testCases,startCode}=req.body;
         const ai = new GoogleGenAI({apiKey:process.env.Gemni_Api_Key});
         async function main() {
         const response = await ai.models.generateContent({
         model: "gemini-3-flash-preview",
         contents: "Explain how AI works in a few words",
         config: {
-         systemInstruction: `Your role:
+        systemInstruction: `Your role:
                            - Help users solve programming problems.
                            - Provide hints instead of full solutions unless the user explicitly asks for the full solution.
                            - Explain compiler errors, runtime errors, and logic mistakes clearly.
@@ -21,14 +22,14 @@ const solveDout=async(req,res)=>{
                            - Focus only on programming, algorithms, and debugging.`
        }
     });
-      res.status(201).send(response.text);
+      res.status(201).json({ message:response.text});
       console.log(response.text);
      }
      main();
     }
     catch (err) {
     console.log(err.message);
-    res.status(500).send("Internal error in ChatBot");
+     res.status(500).json({message:"Internal error in ChatBot"});
   }
 }
 module.exports=solveDout;
