dcbd2f1fe6d56bdd1a390cf8b748e0c701999340
Suraj Yadav <surajyadav2823510@gmail.com>
2026-03-27 00:58:11 +0530

    saveVideoMetadata controller added

--- controllers/videoSection.js
+++ controllers/videoSection.js
@@ -48,6 +48,86 @@ const generateUploadSignature=async(req,res)=>{
     res.status(500).json({error:'Failed to generate upload credntials'})
    }
 };
+const saveVideoMetadata = async (req, res) => {
+    try {
+        const {
+            problemId,
+            cloudinaryPublicId,
+            secureUrl,
+            duration,
+        } = req.body;
 
-module.exports={generateUploadSignature}
+        // through adminmiddleware
+        const userId = req.result._id;
+
+        // first we need to verify that the video is uploaded or not in cloudinary
+        const cloudinaryResource = await cloudinary.api.resource(
+            cloudinaryPublicId,
+            { resource_type: 'video' }
+        );
+
+        if (!cloudinaryResource) {
+            return res.status(400).json({ error: 'Video not found on Cloudinary' });
+        }
+
+        // check if video already exists for this problem and user
+        const existingVideo = await SolutionVideo.findOne({
+            problemId,
+            userId,
+            cloudinaryPublicId
+        });
+
+        if (existingVideo) {
+            return res.status(409).json({ error: 'video already exists' });
+        }
+
+
+        // we generate thumbnailUrl throw public_id
+        // here the cloudinaryResource return a object where the public_id and other information is present
+        const thumbnailUrl = cloudinary.url(
+            cloudinaryResource.public_id,
+            {
+                resource_type: "video",
+                transformation: [
+                    { width: 400, height: 225, crop: "fill" },
+                    { quality: "auto" },
+                    { start_offset: "auto" }
+                ],
+                format: "jpg"
+            }
+        );
+
+
+        // create video solution record
+        const viedoSolution = new SolutionVideo({
+            problemId,
+            userId,
+            cloudinaryPublicId,
+            secureUrl,
+            duration: cloudinaryResource.duration || duration,
+            thumbnailUrl
+        });
+
+        await viedoSolution.save();
+
+
+        res.status(201).json({
+            message: "video solution saved successfully",
+            viedoSolution: {
+                id: viedoSolution._id,
+                thumbnailUrl: viedoSolution.thumbnailUrl,
+                duration: viedoSolution.duration,
+                uploadedAt: viedoSolution.createdAt
+            }
+        });
+
+    }
+    catch (error) {
+        console.error('Error saving video metadata', error);
+        res.status(500).json({ error: 'Failed to save video metadata' });
+    }
+};
+
+
+module.exports={generateUploadSignature,saveVideoMetadata}
 
