f2081bd970fe1b1e75a496b68a57146d0562885d
Suraj Yadav <surajyadav2823510@gmail.com>
2026-06-08 02:41:03 +0530

    feat: make CORS and start script deployment-ready

--- src/index.js
+++ src/index.js
@@ -1,38 +1,38 @@
-const express=require('express'); //(npm i express)
-const app=express();
+const express = require('express'); //(npm i express)
+const app = express();
 require('dotenv').config(); //(npm i dotenv)
-const main=require('../config/db');
-const cookieParser=require('cookie-parser'); //(npm i cookie-parser)
-const authRouter=require('../routes/userAuth');
-const client=require('../config/redis');
-const problemRouter=require('../routes/problemCreator')
-const submitRouter=require('../routes/submit');
-const aiRouter=require('../routes/GemniAi');
+const main = require('../config/db');
+const cookieParser = require('cookie-parser'); //(npm i cookie-parser)
+const authRouter = require('../routes/userAuth');
+const client = require('../config/redis');
+const problemRouter = require('../routes/problemCreator')
+const submitRouter = require('../routes/submit');
+const aiRouter = require('../routes/GemniAi');
 const cors = require("cors");
 
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true
 }));
-const videoRouter=require('../routes/videoCreator');
+const videoRouter = require('../routes/videoCreator');
 app.use(express.json());//It convert req.body json data in java script object because through the req.body the data is come in json formate and we need in javascript formate same as in case of cookiese
 app.use(cookieParser());
 
-app.use("/user",authRouter);
+app.use("/user", authRouter);
 app.use("/problem", problemRouter);
-app.use("/submission",submitRouter);
-app.use("/ai",aiRouter);
-app.use('/video',videoRouter);
+app.use("/submission", submitRouter);
+app.use("/ai", aiRouter);
+app.use('/video', videoRouter);
 main()
-.then(async()=>{
+  .then(async () => {
     console.log("DB connected");
-    await client.connect();   
+    await client.connect();
     console.log("Redis connected");
 
-    app.listen(process.env.PORT,()=>{
-        console.log("Server listening at port : "+process.env.PORT);
+    app.listen(process.env.PORT, () => {
+      console.log("Server listening at port : " + process.env.PORT);
     });
 
-})
-.catch(err => console.log("Error Occured: "+ err));
+  })
+  .catch(err => console.log("Error Occured: " + err));
 
